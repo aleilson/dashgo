3b841f04b85e23282cde7ae35b05bac7fd97c15f
Aleilson Gomes <aleilson.cerqueira@gmail.com>
2021-06-03 11:52:13 -0300

    Add ssr and pagination with list users

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,28 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { theme } from '../styles/theme';
 
 
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 import { SidebarDrawerProvider } from '../context/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
+import { queryClient } from '../services/queryCluent';
 
 if(process.env.NODE_ENV === 'development'){
   makeServer();
 }
 
-const queryCliente = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryCliente}>
+    <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
         <SidebarDrawerProvider>
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,24 +1,37 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue, Spinner } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue, Spinner, Link } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
+import NextLink from "next/link";
+import { useState } from "react";
 import { RiAddLine } from "react-icons/ri";
-import { useQuery } from 'react-query';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryCluent";
 
 export default function UserList(){
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-      const data = await response.json()
-      
-      return data;
-  });
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page)
+  // , {
+  //   initialData: users
+  // });
 
   const isWiderVersion = useBreakpointValue({
     base: false,
     lg: true
   });
+
+  async function handlePrefetchUser(userId: string){
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`)
+
+      return response.data;
+    }, {
+      staleTime: 1000 * 60 * 10 // 10 minutes
+    })
+  }
 
   return (
     <Box>
@@ -30,13 +43,16 @@ export default function UserList(){
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              { !isLoading && isFetching && <Spinner sizw="sm" color="gray.500" ml="4" /> }
+            </Heading>
 
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
           { isLoading ? (
             <Flex justify="center">
@@ -59,7 +75,7 @@ export default function UserList(){
                 </Tr>
               </Thead>
               <Tbody>
-                {data.users.map(user => {
+                {data.users.map((user) => {
                   return (
                     <Tr key={user.id}>
                       <Td px={["4", "4", "6"]}>
@@ -67,7 +83,9 @@ export default function UserList(){
                       </Td>
                       <Td>
                         <Box>
-                          <Text fontWeight="bold">{user.name}</Text>
+                          <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                            <Text fontWeight="bold">{user.name}</Text>
+                          </Link>
                           <Text fontSize="sm" color="gray.300">{user.email}</Text>
                         </Box>
                       </Td>
@@ -78,7 +96,11 @@ export default function UserList(){
               </Tbody>
             </Table>
             
-            <Pagination />
+            <Pagination
+              totalCountOfRegister={data.totalCount}
+              currentPage={page}
+              onPageChange={setPage}
+            />
             </>
           )}
         </Box>
@@ -86,3 +108,13 @@ export default function UserList(){
     </Box>
   );
 }
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const { users, totalCount } = await getUsers(1);
+
+//   return {
+//     props: {
+//       users,
+//     }
+//   }
+// }
